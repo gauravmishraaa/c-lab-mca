11da0d4a3c37af9d67fe428c08c124ee53edbe57
gaurav mishra <151965437+gauravmishraaa@users.noreply.github.com>
2024-10-04 15:49:19 +0530

    Add files via upload

---
+++ vowelconsonent.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+char c;
+printf("enter a character");
+scanf("%c",&c);
+if(c=='a'||'e'||'i'||'o'||'u'){
+    printf("the character is  a vowel");
+
+}
+else{
+    printf("the  character is a consonant");
+
+}
+return 0;
+
+
+
+
+}
